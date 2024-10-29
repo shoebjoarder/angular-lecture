139bc2db6ce84a31de4463c270b1819cf2f114d8
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-10-29 21:29:11 +0100

    Init component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,28 @@
 import { Component } from '@angular/core';
 import { IFruit } from './model/grocery';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  // **********************************************************
-  // * Primitive types ****************************************
-  // **********************************************************
-
-  // * String type
+  // * String
   groceryItem: string = 'apple';
 
-  // * Boolean
-  isAdded: boolean = true;
+  // * Array of strings
+  groceryList: string[] = [
+    'banana', 'oats', 'milk', 'chia seeds'
+  ];
 
-  // * Number
-  countItems: number = 0;
-
-  // **********************************************************
-  // * Special types ******************************************
-  // **********************************************************
-
-  // * HTML/CSS
-  groceryListTitleHTML: any = `<h2 style={{ color: "red" }}>HTML Grocery List</h2>`;
-
-  // **********************************************************
-  // * Complex types ******************************************
-  // **********************************************************
-
-  // * Array
-  groceryList: string[] = ['banana', 'oats', 'milk', 'chia seeds'];
-
-  // * Tuple
-  groceryItemTuple: [string, number] = ['banana', 1.29];
-
-  // * Object
-  groceryItemObject: { fruitName: string; color: string; price: number } = {
-    fruitName: 'banana',
-    color: 'yellow',
-    price: 1.29,
-  };
-
-  // * Interface
+  // * Structured object -> Interface
   groceryListOptions: IFruit[] = [
     { fruitName: 'apple', color: 'red' },
     { fruitName: 'kiwi', color: 'green' },
     { fruitName: 'papaya', color: 'yellow' },
     { fruitName: 'plum', color: 'purple' },
   ];
-
-  // **********************************************************
-  // * Other types ********************************************
-  // **********************************************************
-
-  // * Date
-  groceryPurchaseDate: Date = new Date();
 }
