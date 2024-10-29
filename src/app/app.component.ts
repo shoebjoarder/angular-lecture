8871a2d82b275917559fc0b341421acdd1c27ae6
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-10-29 22:48:01 +0100

    Init directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IFruit } from './model/grocery';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -14,9 +15,7 @@ export class AppComponent {
   groceryItem: string = 'apple';
 
   // * Array of strings
-  groceryList: string[] = [
-    'banana', 'oats', 'milk', 'chia seeds'
-  ];
+  groceryList: string[] = ['banana', 'oats', 'milk', 'chia seeds'];
 
   // * Structured object -> Interface
   groceryListOptions: IFruit[] = [
@@ -25,4 +24,17 @@ export class AppComponent {
     { fruitName: 'papaya', color: 'yellow' },
     { fruitName: 'plum', color: 'purple' },
   ];
+
+  // * ngStyle example
+  addButtonColor: string = 'black';
+
+  // * ngClass example
+  isPrimary: boolean = true;
+  isDisabled: boolean = false;
+
+  // * Function to enter grocery item: ngModel example
+  handleGroceryItemChange(event: Event): void {
+    let inputElement = event.target as HTMLInputElement;
+    this.groceryItem = inputElement.value;
+  }
 }
