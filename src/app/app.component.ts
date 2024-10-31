964fcbeac4c2b5d4c7794e26f5fb486613e7d896
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-10-31 09:26:46 +0100

    Add template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IFruit } from './model/grocery';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
