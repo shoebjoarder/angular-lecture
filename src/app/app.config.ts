5a812f9137ccae6caa004c2062fb1cea34caab67
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-11-06 10:49:33 +0100

    Init dependency injection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     // * Making routing available from app.routes.ts file
     provideRouter(routes),
+    // * Making http available
+    provideHttpClient(),
   ],
 };
--- src/app/pages/news-dynamic/news-dynamic.component.ts
+++ src/app/pages/news-dynamic/news-dynamic.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { NewsPostService } from '../../services/news-post/news-post.service';
+import IPost from '../../model/post';
+import { HttpClient, HttpHandler, HttpXhrBackend } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-news-dynamic',
@@ -11,15 +15,47 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class NewsDynamicComponent implements OnInit {
   paramId: string | null = null;
+  newsPost: IPost | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  // httpHandler: HttpHandler = new HttpXhrBackend({
+  //   build: () => new XMLHttpRequest(),
+  // });
+
+  // // * Without dependency injection
+  // route: ActivatedRoute;
+  // http: HttpClient;
+  // constructor() {
+  //   this.route = new ActivatedRoute();
+  //   this.http = new HttpClient(this.httpHandler);
+  // }
+
+  // // * With dependency injection
+  // constructor(
+  //   private route: ActivatedRoute,
+  //   private http: HttpClient,
+  // ) {}
+
+  // * Dependency injection from Angular 18
+  route = inject(ActivatedRoute);
+  http = inject(HttpClient);
 
   ngOnInit(): void {
     this.paramId = this.route.snapshot.paramMap.get('id');
+    if (this.paramId !== null) {
+      this.getNewsPost(this.paramId).subscribe(
+        (response: IPost) => (this.newsPost = response)
+      );
+    }
 
     // * Alternative using Observables
     // this.route.paramMap.subscribe((params) => {
     //   this.paramId = params.get('id');
     // });
+  }
+
+  getNewsPost(id: string): Observable<IPost> {
+    return this.http.get<IPost>(
+      `https://jsonplaceholder.typicode.com/posts/${id}`
+    );
   }
 }
