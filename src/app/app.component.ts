5ed0e7ec8cb966561ec383848b3612465c281378
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-11-07 10:22:56 +0100

    Refactor grocery list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,12 @@
 import { Component } from '@angular/core';
-import { IFruit } from './model/grocery';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
-// * Import from named export
-// import {FruitbuttonComponent} from './fruitbutton/fruitbutton.component';
-
-// * Import from default export
-import FruitbuttonComponent from './fruitbutton/fruitbutton.component';
+import { GroceryListComponent } from './pages/grocery-list/grocery-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FruitbuttonComponent, CommonModule, FormsModule],
+  imports: [GroceryListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  // * String
-  groceryItem: string = 'apple';
-
-  // * Array of strings
-  groceryList: string[] = ['banana', 'oats', 'milk', 'chia seeds'];
-
-  // * Structured object -> Interface
-  groceryListOptions: IFruit[] = [
-    { fruitName: 'apple', color: 'red' },
-    { fruitName: 'kiwi', color: 'green' },
-    { fruitName: 'papaya', color: 'yellow' },
-    { fruitName: 'plum', color: 'purple' },
-  ];
-
-  // * ngStyle example
-  addButtonColor: string = 'black';
-
-  // * ngClass example
-  isPrimary: boolean = true;
-  isDisabled: boolean = false;
-
-  // * ngModel example
-  // * Function to enter grocery item:
-  handleGroceryItemChange(event: Event): void {
-    let inputElement = event.target as HTMLInputElement;
-    this.groceryItem = inputElement.value;
-  }
-
-  // * Function to add a fruit to the list using groceryItem data
-  handleAddFruit() {
-    if (Boolean(this.groceryItem)) {
-      this.groceryList = [...this.groceryList, this.groceryItem];
-      this.groceryItem = '';
-    }
-  }
-
-  // * Custom function with parameters to add a grocery item to the list
-  handleAddFruitName(fruit: string) {
-    if (Boolean(fruit)) {
-      this.groceryList = [...this.groceryList, fruit];
-    }
-  }
-}
+export class AppComponent {}
---
+++ src/app/pages/grocery-list/grocery-list.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { IFruit } from '../../model/grocery';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// * Import from named export
+// import { FruitbuttonComponent } from '../../fruitbutton/fruitbutton.component';
+
+// * Import from default export
+import FruitbuttonComponent from '../../components/fruitbutton/fruitbutton.component';
+
+@Component({
+  selector: 'app-grocery-list',
+  standalone: true,
+  imports: [FruitbuttonComponent, CommonModule, FormsModule],
+  templateUrl: './grocery-list.component.html',
+  styleUrl: './grocery-list.component.css',
+})
+export class GroceryListComponent {
+  // * String
+  groceryItem: string = 'apple';
+
+  // * Array of strings
+  groceryList: string[] = ['banana', 'oats', 'milk', 'chia seeds'];
+
+  // * Structured object -> Interface
+  groceryListOptions: IFruit[] = [
+    { fruitName: 'apple', color: 'red' },
+    { fruitName: 'kiwi', color: 'green' },
+    { fruitName: 'papaya', color: 'yellow' },
+    { fruitName: 'plum', color: 'purple' },
+  ];
+
+  // * ngStyle example
+  addButtonColor: string = 'black';
+
+  // * ngClass example
+  isPrimary: boolean = true;
+  isDisabled: boolean = false;
+
+  // * ngModel example
+  // * Function to enter grocery item:
+  handleGroceryItemChange(event: Event): void {
+    let inputElement = event.target as HTMLInputElement;
+    this.groceryItem = inputElement.value;
+  }
+
+  // * Function to add a fruit to the list using groceryItem data
+  handleAddFruit() {
+    if (Boolean(this.groceryItem)) {
+      this.groceryList = [...this.groceryList, this.groceryItem];
+      this.groceryItem = '';
+    }
+  }
+
+  // * Custom function with parameters to add a grocery item to the list
+  handleAddFruitName(fruit: string) {
+    if (Boolean(fruit)) {
+      this.groceryList = [...this.groceryList, fruit];
+    }
+  }
+}
